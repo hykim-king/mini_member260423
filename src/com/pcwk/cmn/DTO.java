bbf9d913267642d64c7c8231b0c3ebdc2c4d646a
hykim <hykim22@gmail.com>
2026-04-23 17:09:33 +0900

    최초 작성

---
+++ src/com/pcwk/cmn/DTO.java
@@ -0,0 +1,15 @@
+/**
+ * 파일명: DTO.java <br>
+ * 설 명: 모든 Value Object의 Parent <br>
+ * 작성자: user  <br>
+ * 생성일: 2026-04-23 <br>
+ */
+package com.pcwk.cmn;
+
+/**
+ * Data Transfer Object, 데이터 전송 객체
+ */
+
+public class DTO {
+
+}
